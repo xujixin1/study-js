650cdd89bc4d251917c56f0df15cb804a100a618
xujixin1 <1399706488@qq.c0m>
2017-09-24 20:43:16 +0800

    csrf安全协议

--- egg-example/config/config.default.js
+++ egg-example/config/config.default.js
@@ -9,5 +9,11 @@ module.exports = appInfo => {
   // add your config here
   config.middleware = [];
 
+  config.security = {
+    csrf: {
+      ignoreJSON: true,
+    },
+  };
+
   return config;
 };
