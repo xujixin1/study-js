6ca150c21ae6903831fef5f2064d49b7a233f0dd
xujixin1 <1399706488@qq.c0m>
2017-09-24 16:07:42 +0800

    home服务

--- egg-example/app/service/home.js
+++ egg-example/app/service/home.js
@@ -1,9 +1,27 @@
+/**
+ * 2017-9-24 Xu Jixin
+ */
 'use strict';
 
 module.exports = app => {
   class Home extends app.Service {
     * find() {
       return 'user';
+    }
+    * sina() {
+      return yield app.curl('http://www.sina.com.cn');
+    }
+    * sohu() {
+      return yield app.curl('http://www.sohu.com.cn');
+    }
+    * baidu() {
+      return yield app.curl('http://www.baidu.com.cn');
+    }
+    * test() {
+      return 'abc';
+    }
+    * ppap() {
+      return '123';
     }
   }
   return Home;
