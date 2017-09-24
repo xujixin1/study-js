de3a0c719c32f5e3e2ee86f8ed64711b1929ee65
xujixin1 <1399706488@qq.c0m>
2017-09-24 16:05:55 +0800

    路由

--- egg-example/app/router.js
+++ egg-example/app/router.js
@@ -1,6 +1,13 @@
+/**
+ * 
+ */
 'use strict';
 
 module.exports = app => {
-  app.get('/', 'home.index');
-  app.get('/home', 'name.index');
+  app.get('/sina', 'home.sina');
+  app.get('/sohu', 'home.sohu');
+  app.get('/baidu', 'home.baidu');
+  app.get('/test', 'home.test');
+  app.get('/ppap', 'home.ppap');
+  app.get('/car', 'home.car');
 };
