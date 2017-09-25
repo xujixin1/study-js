6a8841855e37555a0540a229c9fa60681b79f8ae
xujixin1 <1399706488@qq.com>
2017-09-25 21:03:09 +0800

    俩个表的路由

--- egg-example/app/router.js
+++ egg-example/app/router.js
@@ -16,4 +16,12 @@ module.exports = app => {
   app.post('/look', 'home.look');
   app.post('/date', 'home.date');
   app.post('/lodash', 'home.find');
+  app.post('/create', 'test.create');
+  app.post('/delete', 'test.delete');
+  app.post('/get', 'test.get');
+  app.post('/update', 'test.update');
+  app.post('/create1', 'student.create');
+  app.post('/delete1', 'student.delete');
+  app.post('/get1', 'student.get');
+  app.post('/update1', 'student.update');
 };
