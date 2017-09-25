0b379a8d9b6d56e6c29d60a29675e63112a865fb
xujixin1 <1399706488@qq.com>
2017-09-25 21:01:15 +0800

    student表

--- egg-example/app.js
+++ egg-example/app.js
@@ -20,5 +20,18 @@ module.exports = app => {
       });
       yield app.mysql.query(userSchema.toString());
     }
+    const hasStudent = yield app.mysql.query(knex.schema.hasTable('student').toString());
+    if (hasStudent.length === 0) {
+      const studentSchema = knex.schema.createTableIfNotExists('student', function(table) {
+        table.increments();
+        table.integer('sno').notNullable().defaultTo(0);
+        table.string('name').notNullable().defaultTo('');
+        table.integer('age').notNullable().defaultTo(0);
+        table.string('sex').notNullable().defaultTo('');
+        table.timestamp('creat_at').defaultTo(knex.fn.now());
+        table.charset('utf8');
+      });
+      yield app.mysql.query(studentSchema.toString());
+    }
   });
 };
