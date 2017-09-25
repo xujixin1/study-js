2ea6d13557812f3fd878a80be7a6114eff594d24
xujixin1 <1399706488@qq.com>
2017-09-25 21:02:45 +0800

    user表增删改查

--- egg-example/app/service/test.js
+++ egg-example/app/service/test.js
@@ -5,8 +5,42 @@
 
 module.exports = app => {
   class Test extends app.Service {
-    * test() {
-      return '123';
+    * create(param) {
+      try {
+        yield app.mysql.insert('user', param);
+      } catch (e) {
+        this.ctx.logger.error(e);
+        return false;
+      }
+      return true;
+    }
+    * get(req) {
+      let res;
+      try {
+        res = yield app.mysql.get('user', req);
+      } catch (e) {
+        this.ctx.logger.error(e);
+        return false;
+      }
+      return res;
+    }
+    * delete(param) {
+      try {
+        yield app.mysql.delete('user', param);
+      } catch (e) {
+        this.ctx.logger.error(e);
+        return false;
+      }
+      return true;
+    }
+    * update(param) {
+      try {
+        yield app.mysql.update('user', param);
+      } catch (e) {
+        this.ctx.logger.error(e);
+        return false;
+      }
+      return true;
     }
   }
   return Test;
