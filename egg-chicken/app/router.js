39b8bd4031ff87927119aa2b030c186483b3d0ff
xujixin1 <1399706488@qq.com>
2017-09-27 16:11:52 +0800

    egg-chicken

--- egg-chicken/app/router.js
+++ egg-chicken/app/router.js
@@ -1,5 +1,23 @@
+/**
+ * 2017-9-27 Xu Jixin
+ */
 'use strict';
 
 module.exports = app => {
   app.get('/', 'home.index');
+  app.post('/create', 'test.create');
+  app.post('/delete', 'test.delete');
+  app.post('/select', 'test.select');
+  app.post('/update', 'test.update');
+
+  app.post('/create1', 'student.create');
+  app.post('/delete1', 'student.delete');
+  app.post('/select1', 'student.select');
+  app.post('/update1', 'student.update');
+  app.post('/get1', 'student.get');
+
+  app.post('/create2', 'teacher.create');
+  app.post('/delete2', 'teacher.delete');
+  app.post('/select2', 'teacher.select');
+  app.post('/update2', 'teacher.update');
 };
